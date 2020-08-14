67d63ec54b4592393722aa0ecafb05a5d9829fa6
jolean lim <joleanflima@outlook.com>
2020-08-14 14:17:07 -0400

    deploying

--- routes/user-routes.js
+++ routes/user-routes.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const userRouter = express.Router();
 
-const authHelpers = require('../auth/auth-helpers');
+
 const usersController = require('../controllers/user-controller');
+const authHelpers = require('../auth/auth-helpers');
 
 userRouter.get('/', authHelpers.loginRequired, usersController.index);
 userRouter.post('/', usersController.create);
@@ -10,6 +11,6 @@ userRouter.get('/new', authHelpers.loginRedirect, (req, res) => {
     res.render('auth/register');
 });
 
-userRouter.post('/', usersController.create);
+
 
 module.exports = userRouter;
