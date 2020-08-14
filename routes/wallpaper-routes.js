1fca8f51902ae3e1083318a024daf808b680ab73
jolean lim <joleanflima@outlook.com>
2020-08-14 16:30:07 -0400

    stylesheet works

--- routes/wallpaper-routes.js
+++ routes/wallpaper-routes.js
@@ -5,12 +5,10 @@ const wallRouter = express.Router();
 
 wallRouter.get('/', wallController.index);
 wallRouter.post('/', wallController.create);
-// wallRouter.get('/')
 
-// wallRouter.get('/:id)', wallRouter.show, (req, res) => {
-//     res.render('wallpaper/show', {
-//         wallpaper: res.locals.wallpaper,
-//     })
-// });
+
+
+
+
 
 module.exports = wallRouter;
