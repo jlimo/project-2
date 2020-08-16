96439f686e42f8dc88ddf10e65deae07dec0d776
jolean lim <joleanflima@outlook.com>
2020-08-15 23:59:44 -0400

    help

--- routes/wallpaper-routes.js
+++ routes/wallpaper-routes.js
@@ -1,12 +1,29 @@
 const express = require('express');
 const wallController = require('../controllers/wallpaper-controller');
+const authHelpers = require('../auth/auth-helpers');
 const wallRouter = express.Router();
 
 
 wallRouter.get('/', wallController.index);
-wallRouter.post('/', wallController.create);
+wallRouter.post('/', authHelpers.loginRequired, wallController.create);
 
-//wallRouter.get('/:id([0-9]+)', wallController.show, wallController.show, 
+wallRouter.get('/add', authHelpers.loginRequired, (req, res) => {
+    res.render('wallpaper/wallpaper-new');
+});
+
+// wallRouter.get('/:id', wallController.show);
+// wallRouter.get(
+//     '/:id/edit',
+//     authHelpers.loginRequired,
+//     wallController.edit
+// );
+// wallRouter.put('/:id', authHelpers.loginRequired, wallController.update);
+// wallRouter.delete(
+//     '/:id',
+//     authHelpers.loginRequired,
+//     wallController.delete
+// );
+
 
 
 
